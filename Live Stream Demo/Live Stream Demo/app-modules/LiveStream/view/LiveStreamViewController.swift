1e5f40255f9fd88e5d27773f17d80d92c8e1a313
Tushar Gupta <tushar.gupta@wheelseye.com>
2024-12-22 17:52:49 +0530

    basic project setup with mvvm architecture.

---
+++ Live Stream Demo/Live Stream Demo/app-modules/LiveStream/view/LiveStreamViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Live Stream Demo
+//
+//  Created by Tushar on 22/12/24.
+//
+
+import UIKit
+
+class LiveStreamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
